824f4439f7b5b1e5986524cd76999b42942587e0
TheMightyBoop <43588463+TheMightyBoop@users.noreply.github.com>
2021-04-17 02:12:06 -0500

    Monkey

---
+++ Assets/Scripts/Other/SimpleAIInput.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAIInput : MonoBehaviour, IInput
+{
+    public Action<Vector3> OnMovementDirectionInput { get; set; }
+    public Action<Vector2> OnMovementInput { get; set; }
+
+    bool playerDetectionResult = false;
+    public Transform eyesTransform;
+    public Transform playerTransform;
+    public LayerMask playerLayer;
+    public float visionDistance, stoppingDistance = 1.2f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        if (playerDetectionResult)
+        {
+            Gizmos.color = Color.red;
+        }
+        Gizmos.DrawWireSphere(eyesTransform.position, visionDistance);  
+    }
+
+    private void Update()
+    {
+        playerDetectionResult = DetectPlayer();
+        if (playerDetectionResult)
+        {
+            var directionToPlayer = playerTransform.position - transform.position;
+            directionToPlayer = Vector3.Scale(directionToPlayer, Vector3.forward + Vector3.right);
+            if(directionToPlayer.magnitude > stoppingDistance)
+            {
+                directionToPlayer.Normalize();
+                OnMovementInput?.Invoke(Vector2.up);
+                OnMovementDirectionInput?.Invoke(directionToPlayer);
+                return;
+            }
+        }
+        OnMovementInput?.Invoke(Vector2.zero);
+        OnMovementDirectionInput?.Invoke(transform.forward);
+    }
+
+    private bool DetectPlayer()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(eyesTransform.position, visionDistance, playerLayer);
+        foreach(var collider in hitColliders)
+        {
+            playerTransform = collider.transform;
+            return true;
+        }
+
+        playerTransform = null;
+        return false;
+    }
+}
---
+++ Assets/Scripts/Player/CharacterLocomotion.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLocomotion : MonoBehaviour
+{
+    public float jumpHeight;
+    public float gravity;
+    public float stepDown;
+
+    CharacterController controller;
+    Animator animator;
+    Vector2 input;
+
+    Vector3 rootMotion;
+    Vector3 velocity;
+    public bool isJumping;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+
+        animator.SetFloat("InputX", input.x);
+        animator.SetFloat("InputY", input.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+    }
+
+    private void OnAnimatorMove()
+    {
+        rootMotion += animator.deltaPosition;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isJumping)
+        {
+            velocity.y -= gravity * Time.fixedDeltaTime;
+            controller.Move(velocity * Time.fixedDeltaTime);
+            isJumping = !controller.isGrounded;
+            rootMotion = Vector3.zero;
+        }
+        else
+        {
+            controller.Move(rootMotion + Vector3.down * stepDown);
+            rootMotion = Vector3.zero;
+
+            if (!controller.isGrounded)
+            {
+                isJumping = true;
+                velocity = animator.velocity;
+                velocity.y = 0;
+            }
+        }
+    }
+
+    void Jump()
+    {
+        if (!isJumping)
+        {
+            isJumping = true;
+            velocity = animator.velocity;
+            velocity.y = Mathf.Sqrt(2 * gravity * jumpHeight);
+        }
+    }
+}
